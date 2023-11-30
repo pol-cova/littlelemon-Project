126afdf89250aa2c11b4b72789e7a1964c765295
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-11-29 19:21:27 -0600

    Add Menu and Booking models to admin site

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Restaurant api models
+
+# Menu Model 
+# title: varchar(255), price: decimal(10, 2), inventory: int(5) 
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+# Booking Model
+# name: varchar(255), no_of_guests: int(6), BookingDate: datetime
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=0)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
