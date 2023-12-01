63fa1a5d8c373fb796734b33e5d85f772e07648d
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-11-30 21:12:21 -0600

    Add REST framework and create menu endpoints

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('restaurant/menu/', views.MenuItemsViews.as_view(), name='menu'),
+    path('restaurant/menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu_item'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# Models and serializers
+from .models import Menu, Booking
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
     
+
+# Menu API
+class MenuItemsViews(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Single Menu Item API
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
